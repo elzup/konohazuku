787a44c02acabafbd1d0ca4eb79f3e7e0e0e80fe
elzup <guild0105@gmail.com>
2017-05-06 10:18:58 +0900

    Rename project to konohazuku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_readall_session'
+Rails.application.config.session_store :cookie_store, key: '_konohazuku_session'
